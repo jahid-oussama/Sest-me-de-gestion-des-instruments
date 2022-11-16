249ebfc420fd0c6ec4644e4bc0f1c1225621962f
oussama <jahid20oussama@gmail.com>
2022-11-16 11:44:06 +0100

    finished front end

---
+++ first.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS only -->
+<!-- CSS only -->
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
+<!-- BEGIN parsley css-->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/guillaumepotier/Parsley.js@2.9.2/doc/assets/docs.css">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/guillaumepotier/Parsley.js@2.9.2/src/parsley.css">
+<!-- END parsley css-->
+
+    <title>Document</title>
+</head>
+<body>
+                           <!-- back ground image -->
+    <style>
+        body{
+          background-image: url('image/pngegg.png');
+          background-repeat: no-repeat;
+          background-attachment: fixed;
+          background-position: center;
+        }
+      </style>
+                            <!-- back ground image -->
+                            <!-- nav bar image -->
+    <header class="p-3 text-bg-dark d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+        <img src="image/pngegg.png" alt="" class="ps-3"  style=" width:70px">
+        <div class="container ">
+            
+          <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+
+         
+            <!-- <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+              <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
+            </a> -->
+    
+            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+              <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+              <li><a href="#" class="nav-link px-2 text-white">chi7aja</a></li>
+              <li><a href="#" class="nav-link px-2 text-white">chi7aja</a></li>
+              <li><a href="#" class="nav-link px-2 text-white">chi7aja</a></li>
+              <li><a href="#" class="nav-link px-2 text-white">About Us</a></li>
+            </ul>
+    
+            <!-- <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
+              <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
+            </form> -->
+    
+            <div class="text-end">
+              <button type="button" class="btn btn-outline-light me-2 " data-bs-toggle="modal" data-bs-target="#exampleModal" >Login</button>
+              <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#sign-up"  >Sign-up</button>
+            </div>
+          </div>
+        </div>
+                            <!-- nav bar image -->        
+      </header>   
+                                        <!-- side bar  -->
+      <!-- <section class=" vh-100 gradient-custom" id="exampleModal" data-bs-toggle="modal" data-bs-target="#exampleModal" >
+        <div class="modal-dialog  modal-xl ">
+        <div class="modal-content  py-5 h-100">
+          <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+
+              <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                <div class="card-body p-5 text-center">
+      
+                  <div class="mb-md-5 mt-md-4 pb-5">
+      
+                    <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                    <p class="text-white-50 mb-5">Please enter your login and password!</p>
+      
+                    <div class="form-outline form-white mb-4">
+                      <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+                      <label class="form-label" for="typeEmailX">Email</label>
+                    </div>
+      
+                    <div class="form-outline form-white mb-4">
+                      <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+                      <label class="form-label" for="typePasswordX">Password</label>
+                    </div>
+      
+                    <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+      
+                    <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+      
+                    <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                      <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                      <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                      <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                    </div>
+      
+                  </div>
+      
+                  <div>
+                    <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
+                    </p>
+                  </div>
+      
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </section> -->
+                                        <!-- side bar  -->
+                                      <!-- Modal sign-up parsley.js -->
+<form action="" method="post" data-parsley-validate>
+         <div class="modal fade" id="sign-up" tabindex="-1" aria-labelledby="sign-up" aria-hidden="true">
+           <div class="modal-dialog">
+             <div class="modal-content" style="border-radius: 1.5rem;">
+               
+               <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                 <div class="card-body p-5 text-center">
+     
+                   <div class="mb-md-5 mt-md-4 pb-5">
+     
+                     <h2 class="fw-bold mb-2 text-uppercase">sign-up</h2>
+                     <p class="text-white-50 mb-5">Create Account!</p>
+     
+                     <div class="form-outline form-white mb-4">
+                       <input type="text" id="typeEmailX" class="form-control form-control-lg" required/>
+                       <label class="form-label" for="typeEmailX">Username</label>
+                     </div>
+     
+                     <div class="form-outline form-white mb-4">
+                       <input type="email" id="typeEmailX" class="form-control form-control-lg" required/>
+                       <label class="form-label" for="typeEmailX">Email</label>
+                     </div>
+     
+                     <div class="form-outline form-white mb-4">
+                       <input type="password" id="typePasswordy" class="form-control form-control-lg" required/>
+                       <label class="form-label" for="typePasswordX">Password</label>
+                     </div>
+
+                     <div class="form-outline form-white mb-4">
+                      <input type="password" id="typePasswordX" class="form-control form-control-lg" required data-parsley-equalto="#typePasswordy"/>
+                      <label class="form-label" for="typePasswordX">Password</label>
+                    </div>
+     
+                     <!-- <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p> -->
+     
+                     <button class="btn btn-outline-light btn-lg px-5" type="submit">Sign-up</button>
+     
+                     <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                       <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                       <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                       <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                     </div>
+     
+                   </div>
+     
+                   <div>
+                     <p class="mb-0">Already have an account? <a href="#!" class="text-white-50 fw-bold" data-bs-toggle="modal" data-bs-target="#exampleModal" >Login</a>
+                     </p>
+                   </div>
+     
+                 </div>
+               </div>
+             </div>
+           </div>
+         </div>
+</form>
+                                            <!-- Modal sign-up parsley.js -->
+
+                                            <!-- Modal  sign-in parsley.js -->
+<form action="" method="post" data-parsley-validate>
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content" style="border-radius: 1.5rem;">
+            
+            <div class="card bg-dark text-white" style="border-radius: 1rem;">
+              <div class="card-body p-5 text-center">
+      
+                <div class="mb-md-5 mt-md-4 pb-5">
+      
+                  <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                  <p class="text-white-50 mb-5">Please enter your login and password!</p>
+      
+                  <div class="form-outline form-white mb-4">
+                    <input type="email" id="typeEmailX" class="form-control form-control-lg" required/>
+                    <label class="form-label" for="typeEmailX">Email</label>
+                  </div>
+      
+                  <div class="form-outline form-white mb-4">
+                    <input type="password" id="typePasswordX" class="form-control form-control-lg" required />
+                    <label class="form-label" for="typePasswordX">Password</label>
+                  </div>
+      
+                  <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
+      
+                  <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+      
+                  <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                    <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                    <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                    <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                  </div>
+      
+                </div>
+      
+                <div>
+                  <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold" data-bs-toggle="modal" data-bs-target="#sign-up">Sign Up</a>
+                  </p>
+                </div>
+      
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</form>
+                                            <!-- Modal  sign-in parsley.js -->
+</body>
+<!-- JavaScript Bundle with Popper -->
+<!-- JavaScript Bundle with Popper -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/parsley.js/2.9.2/parsley.min.js" integrity="sha512-eyHL1atYNycXNXZMDndxrDhNAegH2BDWt1TmkXJPoGf1WLlNYt08CSjkqF5lnCRmdm3IrkHid8s2jOUY4NIZVQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="script.js"></script>
+</html>
